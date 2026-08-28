f49e1859f1dc70417063377e5a5b8581f220d0ee
ShyamAnand007 <a007shyam@gmail.com>
2026-08-28 12:13:23 +0530

    Streamlit app complete

--- app.py
+++ app.py
@@ -1,17 +1,65 @@
 import streamlit as st
 from api.github_api import *
+from analysis.analyzer import *
 st.title("GITHUB PROFILE ANALYZER")
 
 username=st.text_input("Enter your github username:")
 if st.button("Search"):
-    profile = get_user_profile(username)
-    st.header("Profile")
-    st.image(profile["avatar_url"], width=150)
-    st.subheader(profile.get("name") or username)
+    if username.strip()=="":
+        st.warning("Please enter a Github username")
+    else:
+        profile = get_user_profile(username)
+        if "error" in profile:
+            st.error(profile["error"])
+        else:
+            st.header("Profile")
+            st.image(profile["avatar_url"], width=150)
+            st.subheader(profile.get("name") or username)
+            st.write(profile.get("bio") or "No bio available.")
+            st.write("Followers:", profile.get("followers", 0))
 
-    st.write(profile.get("bio") or "No bio available.")
+            repos = get_user_repo(username)
 
-    st.write("Followers:", profile.get("followers", 0))
+            if isinstance(repos,dict) and "error" in repos:
+                st.error(repos["error"])
+            else:
+
+                total_rep=len(repos)
+                total_stars=calculate_t_stars(repos)
+                total_forks=calculate_t_forks(repos)
+                st.header("Statistics")
+                col1,col2,col3=st.columns(3)
+                with col1:
+                    st.metric("Repositories",total_rep)
+                with col2:
+                    st.metric("Stars",total_stars)
+                with col3:
+                    st.metric("Forks",total_forks)
+
+                activity=activity_score(repos)
+                st.subheader("Activity Score")
+                st.write(activity)
+                profile_s=profile_score(profile,repos)
+                st.subheader("Profile Score")
+                st.write(profile_s)
+
+                st.header("Repositories")
+                if len(repos)==0:
+                    st.info("No public repositories found")
+                for repo in repos:
+                    st.subheader(repo.get("name"))
+                    st.write(
+                        repo.get("description") or "No description available"
+                    )
+                    st.write("Stars:",repo.get("stargazers_count",0))
+                    st.write("Forks:",repo.get("forks_count",0))
+                    st.write("Language:",repo.get("language") or "Unknown")
+
+                    st.divider()
+
+
+
+    
 
 
 
